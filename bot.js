3a4ced37f0c058ed5d7752d13bdcae3449eb470d
mantanterindah <tenuz99@gmail.com>
2017-12-28 13:07:41 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+bot.on('ready', async () => {
+	console.log('Bot Starting!!')
+});
+
+bot.on('message', message => {
+	console.log(`${message.content}`)
+});
+
+bot.on('message', message => {
+	const swearWords = ["keciduk"]
+	if (swearWords.some(word => message.content.includes(word)) ) {
+		bot.users.get('309800585438298122').send('Ada yg Kena Rajia om')
+	}
+	
+});
+
+process.on('unhandledRejection', error => {
+  console.error(`Uncaught Promise Error: \n${error.stack}`);
+});
+
+bot.login(process.env.BOT_TOKEN);
