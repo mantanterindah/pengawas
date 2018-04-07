e62587d4ce515be39c2bc0b1ef3d7d02d22cae90
mantanterindah <tenuz99@gmail.com>
2018-04-07 21:32:00 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,25 +20,25 @@ bot.on('message', message => {
 	
 });
 
-bot.on('message', message => {
-	const swearWords1 = ["Connected", "Disconnected"]
-	if (message.author.id === '395817062519406592') return;
-	if (swearWords1.some(word => message.content.includes(word)) ) {
-		const id = "397923744523354114";
-		const channel = bot.channels.get(id);
-		channel.send(message.content)
-		
-	}
+//bot.on('message', message => {
+//	const swearWords1 = ["Connected", "Disconnected"]
+//	if (message.author.id === '395817062519406592') return;
+//	if (swearWords1.some(word => message.content.includes(word)) ) {
+//		const id = "397923744523354114";
+//		const channel = bot.channels.get(id);
+//		channel.send(message.content)
+//		
+//	}
 	
-});
+//});
 
 bot.on('message', message => {
 	const swearWords2 = ["inventory", "sold:"]
 	if (message.author.id === '395817062519406592') return;
 	if (swearWords2.some(word => message.content.includes(word)) ) {
-		const id = "397923602508414978";
+		const id = "432182132660502553";
 		const channel = bot.channels.get(id);
-		channel.send('<@!309800585438298122>' + message.content)
+		channel.send(message.content)
 		
 	}
 	
@@ -48,25 +48,25 @@ bot.on('message', message => {
 	const swearWords3 = ["From:","warning"]
 	if (message.author.id === '395817062519406592') return;
 	if (swearWords3.some(word => message.content.includes(word)) ) {
-		const id = "397924036614553600";
+		const id = "432182622639095809";
 		const id2 = "309800585438298122"
 		const channel = bot.channels.get(id);
-		channel.send('<@!309800585438298122>' + message.content)
+		channel.send(message.content)
 		
 		
 	}
 	
 });
 
-bot.on('message', message => {
-	const swearWords4 = ["udahan"]
-	if (message.author.id === '395817062519406592') return;
-	if (swearWords4.some(word => message.content.includes(word)) ) {
-		bot.users.get('324777613723697152').send(message.content)
+//bot.on('message', message => {
+//	const swearWords4 = ["udahan"]
+//	if (message.author.id === '395817062519406592') return;
+//	if (swearWords4.some(word => message.content.includes(word)) ) {
+//		bot.users.get('324777613723697152').send(message.content)
 		
-	}
+//	}
 	
-});
+//});
 
 
 process.on('unhandledRejection', error => {
